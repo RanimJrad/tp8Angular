47ad1082aedf8d47db0c40e5d18dc8a217bc68a4
RanimJrad <Ranimjrad0@gmail.com>
2023-11-20 20:39:44 +0100

    angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SupermarketComponent } from './supermarket/supermarket.component';
+import { AddComponent } from './add/add.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateSupermarketComponent } from './update-supermarket/update-supermarket.component';
+import { LoginComponent } from './login/login.component';
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SupermarketComponent,
+    AddComponent,
+    UpdateSupermarketComponent,
+    LoginComponent,
+    ForbiddenComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
